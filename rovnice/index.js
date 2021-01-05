c797c8be7d9cdf0276b4f3b69e0182d77d4127c1
Ondřej Vondra <ondrej.vondra@live.com>
2021-01-05 19:26:37 +0100

    added editor

--- rovnice/index.js
+++ rovnice/index.js
@@ -1,19 +1,3 @@
-document.querySelectorAll('.draggable').forEach(item => {
-  item.addEventListener('dragstart', event => {
-    document.querySelectorAll('.draggable').forEach(di => {
-      if (di !== item) {
-        di.style.pointerEvents = 'none';
-      }
-    });
-    event.dataTransfer.setData('text/plain', item.id);
-  });
-  item.addEventListener('dragend', event => {
-    document.querySelectorAll('.draggable').forEach(di => {
-      di.style.pointerEvents = 'auto';
-    });
-  });
-});
-
 document.querySelectorAll('.droptarget').forEach(item => {
   item.addEventListener('dragover', event => {
     event.preventDefault();
@@ -31,3 +15,55 @@ document.querySelectorAll('.droptarget').forEach(item => {
     item.appendChild(elem);
   });
 });
+
+const createDraggableElement = (id) => {
+  const el = document.createElement('div');
+  el.id = id;
+  el.classList.add('draggable');
+  el.setAttribute('draggable', 'true');
+  el.addEventListener('dragstart', event => {
+    document.querySelectorAll('.draggable').forEach(di => {
+      if (di !== el) {
+        di.style.pointerEvents = 'none';
+      }
+    });
+    event.dataTransfer.setData('text/plain', el.id);
+  });
+  el.addEventListener('dragend', event => {
+    document.querySelectorAll('.draggable').forEach(di => {
+      di.style.pointerEvents = 'auto';
+    });
+  });
+  return el;
+};
+
+const setEquation = (equation) => {
+  const parseSide = (side, left) => {
+
+    const rx = /(?:[\+\-]\s*)?[0-9a-z]+/g;
+    const parts = side.match(rx);
+
+    const parent = document.querySelector('.droptarget.' + (left ? 'left' : 'right'));
+
+    parent.querySelectorAll('.draggable').forEach(dr => dr.remove());
+
+    parts.forEach((p, i) => {
+      const el = createDraggableElement((left ? 'left' : 'right') + i);
+      el.classList.add(((p.startsWith('-')+left) % 2) == 0 ? 'negative' : 'positive');
+      el.textContent = p.replace(/[\+\-]/g, '');
+      parent.appendChild(el);
+    });
+  };
+
+  const sides = equation.replace(/\s/g, '').split('=');
+  parseSide(sides[0], true);
+  parseSide(sides[1], false);
+};
+
+document.querySelectorAll('#enter').forEach(item => {
+  item.addEventListener('click', event => {
+    setEquation(document.getElementById('equation').value);
+  });
+});
+
+setEquation('x+20=50');
